6b21fb8ab14d21f182787a91f274aa31bb2ff3eb
Joe F. Chicago <pierregiddio@gmail.com>
2015-08-17 23:43:20 +0200

    nodejs server

---
+++ server.js
@@ -0,0 +1,13 @@
+// server
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('hello');
+});
+
+app.listen(3000);
+console.log('startup');
+
+
