f2b2ed52c8d1ba93036d32f9ebb050b11e444bfa
Joe F. Chicago <pierregiddio@gmail.com>
2015-08-20 13:35:34 +0200

    routes pass test for get

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,50 @@ var express = require('express')
   , router = express.Router()
   , Marque = require('../models/marque')
   , Vehicule = require('../models/vehicule')
+var app = express()
+
 
 //routing
 
-router.use('/api/marques', require('./marques'))
-router.use('/api/vehicules', require('./vehicules'))
+// route middleware that will happen on every request
+router.use(function(req, res, next) {
+
+    // log each request to the console
+    console.log(req.method, req.url);
+
+    // continue doing what we were doing and go to the route
+    next();
+});
+
+// API
+
+router.get('/', function(req, res) {
+    res.json({ message: 'you got a response' });
+});
+
+
+router.post('/marques', function (req, res) {
+  // console.log(name = req.body.name);
+  res.send('hello');
+
+  // call to method Create
+  // Marque.create(name, function (err, marque) {
+  //   res.send(200, name + " is stored");
+  // })
+})
+
+
+router.get('/marques', function(req, res) {
+    res.json({ message: 'you got a response for marques' });
+});
+
+
+
+router.use('/api', router);
+
+// router.use('/marques', require('./marques'))
+// router.use('/vehicules', require('./vehicules'))
+
+
 
 module.exports = router
--- controllers/marques.js
+++ controllers/marques.js
@@ -1,15 +1,52 @@
 var express = require('express');
+var app = express()
 var router = express.Router();
+var bodyParser = require('body-parser');
+// var Marque = require('../models/marques')
+
+
+app.use(bodyParser.json())
 
 //Routings
-
-router.post('/api/marques', function (req, res) {
+// app.use('/api', router);
+router.post('/marques', function (req, res) {
   console.log(name = req.body.name);
+
   // call to method Create
   Marque.create(name, function (err, marque) {
-    res.redirect('/')
+    res.send(200, name + " is stored");
   })
 })
+    .post(function(req, res) {
+        console.log('processing');
+        res.send('processing the login form!');
+    });
+
+
+router.get('/marques', function(req, res) {
+    res.json({ message: 'you got a response for marques' });
+});
+
+router.use('/api', router);
+
+// ROUTES
+// ==============================================
+
+// app.route('/')
+
+//     // show the form (GET http://localhost:8080/login)
+//     .get(function(req, res) {
+//         res.send('this is the login form');
+//     })
+
+//     // process the form (POST http://localhost:8080/login)
+//     .post(function(req, res) {
+//         console.log('processing');
+//         res.send('processing the login form!');
+//     });
+
+// ...
+
 
 
 
