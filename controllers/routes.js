d14228673ce6171c2fa57107c7874a16a9833c46
Joe F. Chicago <pierregiddio@gmail.com>
2015-08-25 02:18:45 +0200

    clean routes

--- controllers/routes.js
+++ controllers/routes.js
@@ -7,23 +7,23 @@ var marques = require('../controllers/marques')
   , queries = require('../controllers/queries')
 
 
-router.get('/', function(req, res) { res.json({ message: 'API is talking' });});
+router.get('/', function(req, res) { res.json({ message: 'API is talking' });})
 
 // CRUD marques routes
 
-router.get('/marques', marques.index);
-router.get('/marques/:id', marques.show);
-router.post('/marques', marques.create);
-router.put('/marques/:id', marques.update);
-router.delete('/marques/:id', marques.delete);
+router.get('/marques', marques.index)
+router.get('/marques/:id', marques.show)
+router.post('/marques', marques.create)
+router.put('/marques/:id', marques.update)
+router.delete('/marques/:id', marques.delete)
 
 // CRUD vehicules routes
 
-router.get('/vehicules', vehicules.index);
-router.get('/vehicules/:id', vehicules.show);
-router.post('/vehicules', vehicules.create);
-router.put('/vehicules/:id', vehicules.update);
-router.delete('/vehicules/:id', vehicules.delete);
+router.get('/vehicules', vehicules.index)
+router.get('/vehicules/:id', vehicules.show)
+router.post('/vehicules', vehicules.create)
+router.put('/vehicules/:id', vehicules.update)
+router.delete('/vehicules/:id', vehicules.delete)
 
 
 // Queries routes
@@ -31,5 +31,5 @@ router.delete('/vehicules/:id', vehicules.delete);
 router.get('/vehicules/marque/:marque', queries.indexVehiculesByMarque)
 
 
-router.use('/api', router);
+router.use('/api', router)
 module.exports = router
